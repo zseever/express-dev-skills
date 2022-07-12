1e4e77d94c78103722adaa7da7822c8ed297b1b8
zseever <zseever@gmail.com>
2022-07-12 16:40:11 -0700

    css & html adjustments

--- models/skills.js
+++ models/skills.js
@@ -1,8 +1,8 @@
 const skills = [
-    {id:235367, desc: 'Arrow Functions'},
-    {id:352608, desc: 'Ternary Operators'},
-    {id:520938, desc: 'DOM Manipulation'},
-    {id:227753, desc: 'Flexbox & Grid'}
+    {id:235367, desc: 'Arrow Functions', rating: '8/10'},
+    {id:352608, desc: 'Ternary Operators', rating: '9/10' },
+    {id:520938, desc: 'DOM Manipulation', rating: '7.5/10'},
+    {id:227753, desc: 'Flexbox & Grid', rating: '6/10'}
 ]
 
 module.exports = {
